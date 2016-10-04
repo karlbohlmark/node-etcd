73712ac76555cfdd337f2aff2ce0239b03aae129
Karl Böhlmark <karl.bohlmark@netinsight.se>
2016-10-04 15:46:45 +0200

    typings

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,20 @@
-export default class Etcd {
-    set(path: string, options: any): void
-    get(path: string, options: any): void
-    mkdir(path: string, options: any): void
-    rmdir(path: string, options: any): void
-    del(path: string, options: any): void
-    watcher(path: string, options: any): void
+import { EventEmitter } from 'events'
+
+export = Etcd
+
+declare function Etcd(urls: Array<string>, options: any);
+
+declare namespace Etcd {
+    export interface Etcd {
+        setAsync(path: string, value: string, options: any): Promise<void>
+        getAsync(path: string, options: any): Promise<any>
+        mkdirAsync(path: string, options: any): Promise<void>
+        rmdirAsync(path: string, options: any): Promise<void>
+        delAsync(path: string, options: any): Promise<void>
+        rawAsync(method: string, path: string, index?: any, options?: any): Promise<any>
+        watcher(path: string, index?: number, options?: any): EtcdWatcher
+    }
+    export class EtcdWatcher extends EventEmitter {
+        stop(): void
+    }    
 }
